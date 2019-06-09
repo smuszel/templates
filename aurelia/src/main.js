5d1f7ebc310a67aa4471e15e1125f65e65c0876a
smuszel <stanislaw.muszel@gmail.com>
2019-06-09 19:51:53 +0200

    remove au template

--- aurelia/src/main.js
+++
@@ -1,8 +0,0 @@
-import { PLATFORM } from 'aurelia-framework';
-import './main.css';
-
-export const configure = async aurelia => {
-    aurelia.use.defaultBindingLanguage().defaultResources();
-    await aurelia.start();
-    await aurelia.setRoot(PLATFORM.moduleName('app'));
-}
